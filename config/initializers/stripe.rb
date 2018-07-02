f166a1a2c23e9b1808245bd1d4bbcf901585c079
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-02 17:24:42 +1000

    add mailgun

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -4,13 +4,3 @@ Rails.configuration.stripe = {
 }
 
 Stripe.api_key = Rails.configuration.stripe[:secret_key]
-
-# config_files = ['foodora_keys.yml']
-#
-# config_files.each do |file_name|
-#   file_path = File.join(Rails.root, 'config', file_name)
-#   config_keys = HashWithIndifferentAccess.new(YAML.safe_load(IO.read(file_path)))[Rails.env]
-#   config_keys.each do |k, v|
-#     ENV[k.upcase] ||= v
-#   end
-# end
