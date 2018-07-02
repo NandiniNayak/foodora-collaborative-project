d5b3ae5440bb7f2e9ede60bda825c4440267052b
arthurtruong <arthurtruong.work@gmail.com>
2018-07-02 14:44:02 +1000

    Error handling

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  get 'comment/index' => 'comment#index'
+
   root 'home#page'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
