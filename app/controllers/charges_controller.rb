396029b61ce96386a60362b7f14fa72ae6c92f41
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-02 15:03:36 +1000

    stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -14,8 +14,9 @@ class ChargesController < ApplicationController
       customer: customer.id,
       amount: @amount,
       description: 'Rails Stripe customer',
-      currency: 'usd'
+      currency: 'aud'
     )
+
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
