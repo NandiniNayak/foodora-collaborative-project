dc274ae33c3317abd6f898f1f6599df2b9271dd3
Andr3w-Pham <andrew.pham2492@gmail.com>
2018-07-04 15:36:30 +1000

    heroku test

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
 require_relative 'boot'
 
 require 'rails/all'
-config.serve_static_assets = true
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +10,7 @@ module FoodoraCollaborativeProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.serve_static_assets = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
